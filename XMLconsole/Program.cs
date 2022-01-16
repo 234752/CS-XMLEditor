791d1e8d10abd88f0214018cc4f5d7f672e8e42b
Patryk <patryk.gielcz@gmail.com>
2022-01-16 18:29:12 +0100

    improved layout visibility

--- XMLconsole/Program.cs
+++ XMLconsole/Program.cs
@@ -19,7 +19,7 @@ namespace editor
 
 
             try { document.Validate(schemas, null); }
-            catch ( XmlSchemaException xsd)
+            catch (XmlSchemaException xsd)
             {
                 return false;
             }
@@ -36,15 +36,30 @@ namespace editor
             {
                 
                 var name = document.XPathSelectElements("/n:COURSES_LIST/n:COURSES/n:COURSE/n:NAME | /n:COURSES_LIST/n:COURSES/n:COURSE/n:POLISH_NAME", baseNamespace).ElementAt(i).Value;
+                string s = "                                                  ";
+                name += s.Substring(name.Length);
                 var id = document.XPathSelectElements("/n:COURSES_LIST/n:COURSES/n:COURSE/n:ID", baseNamespace).ElementAt(i).Value;
+                id += "    ";
                 var ECTS = document.XPathSelectElements("/n:COURSES_LIST/n:COURSES/n:COURSE/n:ECTS", baseNamespace).ElementAt(i).Value;
+                s = "      ";
+                ECTS+= s.Substring(ECTS.Length);
                 var lectures = document.XPathSelectElements("/n:COURSES_LIST/n:COURSES/n:COURSE/n:LECTURE_H", baseNamespace).ElementAt(i).Value;
+                s = "        ";
+                lectures+=s.Substring(lectures.Length);
                 var tutorials = document.XPathSelectElements("/n:COURSES_LIST/n:COURSES/n:COURSE/n:TUTORIAL_H", baseNamespace).ElementAt(i).Value;
+                s = "        ";
+                tutorials +=s.Substring(tutorials.Length);
                 var labs = document.XPathSelectElements("/n:COURSES_LIST/n:COURSES/n:COURSE/n:LABORATORY_H", baseNamespace).ElementAt(i).Value;
+                s = "        ";
+                labs+=s.Substring(labs.Length);
                 var date = document.XPathSelectElements("/n:COURSES_LIST/n:COURSES/n:COURSE/n:GRADING_DATE", baseNamespace).ElementAt(i).Value;
+                s = "              ";
+                date+=s.Substring(date.Length);
                 var weight = document.XPathSelectElements("/n:COURSES_LIST/n:COURSES/n:COURSE/n:WEIGHT", baseNamespace).ElementAt(i).Value;
+                s = "        ";
+                weight+=s.Substring(weight.Length);
                 i++;
-                Console.WriteLine(name + id + ECTS + lectures + tutorials + labs + date + weight);
+                Console.WriteLine(name + id  + ECTS  + lectures  + tutorials  + labs  + date  + weight);
             }
             
 
