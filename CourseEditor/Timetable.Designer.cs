af0e896fb4006deaffe04bdcb7c7c8911b9053f1
Patryk <patryk.gielcz@gmail.com>
2022-01-17 21:45:53 +0100

    adding courses works

--- CourseEditor/Timetable.Designer.cs
+++ CourseEditor/Timetable.Designer.cs
@@ -48,6 +48,9 @@
             this.tutInput = new System.Windows.Forms.TextBox();
             this.labInput = new System.Windows.Forms.TextBox();
             this.dateInput = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.semInput = new System.Windows.Forms.TextBox();
+            this.semLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // authorLabel
@@ -71,7 +74,7 @@
             // nameLabel
             // 
             this.nameLabel.AutoSize = true;
-            this.nameLabel.Location = new System.Drawing.Point(41, 112);
+            this.nameLabel.Location = new System.Drawing.Point(115, 112);
             this.nameLabel.Name = "nameLabel";
             this.nameLabel.Size = new System.Drawing.Size(41, 15);
             this.nameLabel.TabIndex = 2;
@@ -80,7 +83,7 @@
             // idLabel
             // 
             this.idLabel.AutoSize = true;
-            this.idLabel.Location = new System.Drawing.Point(352, 112);
+            this.idLabel.Location = new System.Drawing.Point(429, 112);
             this.idLabel.Name = "idLabel";
             this.idLabel.Size = new System.Drawing.Size(18, 15);
             this.idLabel.TabIndex = 3;
@@ -89,7 +92,7 @@
             // ectsLabel
             // 
             this.ectsLabel.AutoSize = true;
-            this.ectsLabel.Location = new System.Drawing.Point(436, 112);
+            this.ectsLabel.Location = new System.Drawing.Point(509, 112);
             this.ectsLabel.Name = "ectsLabel";
             this.ectsLabel.Size = new System.Drawing.Size(33, 15);
             this.ectsLabel.TabIndex = 4;
@@ -98,7 +101,7 @@
             // lecLabel
             // 
             this.lecLabel.AutoSize = true;
-            this.lecLabel.Location = new System.Drawing.Point(475, 112);
+            this.lecLabel.Location = new System.Drawing.Point(548, 112);
             this.lecLabel.Name = "lecLabel";
             this.lecLabel.Size = new System.Drawing.Size(66, 15);
             this.lecLabel.TabIndex = 5;
@@ -107,7 +110,7 @@
             // tutLabel
             // 
             this.tutLabel.AutoSize = true;
-            this.tutLabel.Location = new System.Drawing.Point(547, 112);
+            this.tutLabel.Location = new System.Drawing.Point(620, 112);
             this.tutLabel.Name = "tutLabel";
             this.tutLabel.Size = new System.Drawing.Size(71, 15);
             this.tutLabel.TabIndex = 6;
@@ -116,7 +119,7 @@
             // labLabel
             // 
             this.labLabel.AutoSize = true;
-            this.labLabel.Location = new System.Drawing.Point(624, 112);
+            this.labLabel.Location = new System.Drawing.Point(697, 112);
             this.labLabel.Name = "labLabel";
             this.labLabel.Size = new System.Drawing.Size(91, 15);
             this.labLabel.TabIndex = 7;
@@ -125,7 +128,7 @@
             // dateLabel
             // 
             this.dateLabel.AutoSize = true;
-            this.dateLabel.Location = new System.Drawing.Point(721, 112);
+            this.dateLabel.Location = new System.Drawing.Point(794, 112);
             this.dateLabel.Name = "dateLabel";
             this.dateLabel.Size = new System.Drawing.Size(82, 15);
             this.dateLabel.TabIndex = 8;
@@ -134,7 +137,7 @@
             // weightLabel
             // 
             this.weightLabel.AutoSize = true;
-            this.weightLabel.Location = new System.Drawing.Point(809, 112);
+            this.weightLabel.Location = new System.Drawing.Point(882, 112);
             this.weightLabel.Name = "weightLabel";
             this.weightLabel.Size = new System.Drawing.Size(50, 15);
             this.weightLabel.TabIndex = 9;
@@ -159,65 +162,94 @@
             // 
             // numInput
             // 
-            this.numInput.Location = new System.Drawing.Point(13, 75);
+            this.numInput.Location = new System.Drawing.Point(12, 74);
             this.numInput.Name = "numInput";
-            this.numInput.Size = new System.Drawing.Size(22, 23);
+            this.numInput.Size = new System.Drawing.Size(23, 23);
             this.numInput.TabIndex = 12;
             // 
             // nameInput
             // 
-            this.nameInput.Location = new System.Drawing.Point(41, 75);
+            this.nameInput.Location = new System.Drawing.Point(115, 74);
             this.nameInput.Name = "nameInput";
             this.nameInput.Size = new System.Drawing.Size(308, 23);
             this.nameInput.TabIndex = 13;
             // 
             // idInput
             // 
-            this.idInput.Location = new System.Drawing.Point(355, 75);
+            this.idInput.Location = new System.Drawing.Point(429, 74);
             this.idInput.Name = "idInput";
             this.idInput.Size = new System.Drawing.Size(75, 23);
             this.idInput.TabIndex = 14;
             // 
             // ectsInput
             // 
-            this.ectsInput.Location = new System.Drawing.Point(436, 75);
+            this.ectsInput.Location = new System.Drawing.Point(509, 74);
             this.ectsInput.Name = "ectsInput";
-            this.ectsInput.Size = new System.Drawing.Size(38, 23);
+            this.ectsInput.Size = new System.Drawing.Size(33, 23);
             this.ectsInput.TabIndex = 15;
             // 
             // lecInput
             // 
-            this.lecInput.Location = new System.Drawing.Point(480, 75);
+            this.lecInput.Location = new System.Drawing.Point(548, 74);
             this.lecInput.Name = "lecInput";
-            this.lecInput.Size = new System.Drawing.Size(61, 23);
+            this.lecInput.Size = new System.Drawing.Size(66, 23);
             this.lecInput.TabIndex = 16;
             // 
             // tutInput
             // 
-            this.tutInput.Location = new System.Drawing.Point(547, 75);
+            this.tutInput.Location = new System.Drawing.Point(620, 74);
             this.tutInput.Name = "tutInput";
             this.tutInput.Size = new System.Drawing.Size(71, 23);
             this.tutInput.TabIndex = 17;
             // 
             // labInput
             // 
-            this.labInput.Location = new System.Drawing.Point(624, 75);
+            this.labInput.Location = new System.Drawing.Point(697, 74);
             this.labInput.Name = "labInput";
             this.labInput.Size = new System.Drawing.Size(91, 23);
             this.labInput.TabIndex = 18;
             // 
             // dateInput
             // 
-            this.dateInput.Location = new System.Drawing.Point(721, 75);
+            this.dateInput.Location = new System.Drawing.Point(794, 74);
             this.dateInput.Name = "dateInput";
             this.dateInput.Size = new System.Drawing.Size(82, 23);
             this.dateInput.TabIndex = 19;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(882, 74);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(50, 23);
+            this.addButton.TabIndex = 20;
+            this.addButton.Text = "ADD";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // semInput
+            // 
+            this.semInput.Location = new System.Drawing.Point(48, 74);
+            this.semInput.Name = "semInput";
+            this.semInput.Size = new System.Drawing.Size(61, 23);
+            this.semInput.TabIndex = 21;
+            // 
+            // semLabel
+            // 
+            this.semLabel.AutoSize = true;
+            this.semLabel.Location = new System.Drawing.Point(48, 112);
+            this.semLabel.Name = "semLabel";
+            this.semLabel.Size = new System.Drawing.Size(61, 15);
+            this.semLabel.TabIndex = 22;
+            this.semLabel.Text = "SEMESTER";
             // 
             // Timetable
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1357, 641);
+            this.Controls.Add(this.semLabel);
+            this.Controls.Add(this.semInput);
+            this.Controls.Add(this.addButton);
             this.Controls.Add(this.dateInput);
             this.Controls.Add(this.labInput);
             this.Controls.Add(this.tutInput);
@@ -267,5 +299,8 @@
         private TextBox tutInput;
         private TextBox labInput;
         private TextBox dateInput;
+        private Button addButton;
+        private TextBox semInput;
+        private Label semLabel;
     }
 }
--- CourseEditor/Timetable.cs
+++ CourseEditor/Timetable.cs
@@ -8,6 +8,7 @@ namespace CourseEditor
 {
     public partial class Timetable : Form
     {
+        public XDocument document;
         public Timetable()
         {
             InitializeComponent();
@@ -17,6 +18,7 @@ namespace CourseEditor
         {
             this.authorLabel.Text = "AUTHOR";
             this.numLabel.Text = "No";
+            this.semLabel.Text = "SEMESTER";
             this.nameLabel.Text = "NAME";
             this.idLabel.Text = "ID";
             this.ectsLabel.Text = "ECTS";
@@ -27,7 +29,7 @@ namespace CourseEditor
             this.weightLabel.Text = "WEIGHT";
         }
 
-        private void DisplayCourses(XDocument document)
+        private void DisplayCourses()
         {
             XNamespace ns = "timetable.pl";
             var baseNamespace = new XmlNamespaceManager(new NameTable());
@@ -40,6 +42,8 @@ namespace CourseEditor
             foreach (XElement el in document.XPathSelectElements("/n:COURSES_LIST/n:COURSES/n:COURSE", baseNamespace))
             {
                 this.numLabel.Text += "\n" + document.Element(ns + "COURSES_LIST").Element(ns + "COURSES").Descendants(ns + "COURSE").ElementAt(i).Attribute("nr").Value.Substring(1);
+
+                this.semLabel.Text += "\n" + document.Element(ns + "COURSES_LIST").Element(ns + "COURSES").Descendants(ns + "COURSE").ElementAt(i).Attribute("semID").Value.Substring(1);
 
                 this.nameLabel.Text += "\n"+ document.XPathSelectElements("/n:COURSES_LIST/n:COURSES/n:COURSE/n:NAME | /n:COURSES_LIST/n:COURSES/n:COURSE/n:POLISH_NAME", baseNamespace).ElementAt(i).Value;
 
@@ -62,7 +66,7 @@ namespace CourseEditor
             }
         }
 
-        private bool ValidateDocument(XDocument document)
+        private bool ValidateDocument()
         {
             XmlSchemaSet schemas = new XmlSchemaSet();
             schemas.Add(null, "format.xsd");
@@ -77,16 +81,68 @@ namespace CourseEditor
             return true;
         }
 
+        private bool AddCourse()
+        {
+            string sem0 = this.semInput.Text;
+            string name0 = this.nameInput.Text;
+            string id0 = this.idInput.Text;
+            int ects0 = int.Parse(this.ectsInput.Text);
+            int lec0 = int.Parse(this.lecInput.Text);
+            int tut0 = int.Parse(this.tutInput.Text);
+            int lab0 = int.Parse(this.labInput.Text);
+            string date0 = this.dateInput.Text;
+            double w = ects0 / 30.0;
+            string weight0 = w.ToString("F2");
+
+            XNamespace ns = "timetable.pl";
+
+            string n0 = document.Element(ns + "COURSES_LIST").Element(ns + "COURSES").Descendants(ns + "COURSE").Last().Attribute("nr").Value;
+            int number0 = int.Parse(n0.Substring(1)) + 1;
+
+            document.Element(ns + "COURSES_LIST").Element(ns + "COURSES").Add(
+            new XElement(ns + "COURSE",
+                new XAttribute("nr", "C" + number0),
+                new XAttribute("semID", "S" + sem0),
+                new XElement(ns + "NAME", name0),
+                new XElement(ns + "ID", id0),
+                new XElement(ns + "ECTS", ects0),
+                new XElement(ns + "LECTURE_H", lec0),
+                new XElement(ns + "TUTORIAL_H", tut0),
+                new XElement(ns + "LABORATORY_H", lab0),
+                new XElement(ns + "GRADING_DATE", new XAttribute("graded", true), date0),
+                new XElement(ns + "WEIGHT", weight0)
+                ));
+
+            
+
+
+
+            if (!ValidateDocument()) document.Element(ns + "COURSES_LIST").Element(ns + "COURSES").Descendants(ns + "COURSE").ElementAt(number0 - 1).Remove();
+
+            
+
+            return false;                                               //DELETE THIS LATER
+        }
+
         private void loadButton_Click(object sender, EventArgs e)
         {
             string filename = this.fileInput.Text;
-            XDocument document = XDocument.Load(filename);
+            document = XDocument.Load(filename);
 
-            if (ValidateDocument(document))
+            if (ValidateDocument())
             {
                 ClearLabels();
-                DisplayCourses(document);
+                DisplayCourses();
             }
         }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            AddCourse();
+            ClearLabels();
+            DisplayCourses();
+        }
+
+
     }
 }
